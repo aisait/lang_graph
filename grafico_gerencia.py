877c84e1cc1c6648a6fbfd3852ba6bb9b2b3950d
langgraph-aisa <joseardon@aisa.com.gt>
2026-07-06 17:05:08 -0600

    Delete grafico_gerencia.py

--- grafico_gerencia.py
+++
@@ -1,5 +0,0 @@
-from langgraph.checkpoint.memory import MemorySaver
-from agent_graph import create_graph
-
-# Instancia en memoria exclusiva para visualización de gerencia
-graph = create_graph(MemorySaver())
